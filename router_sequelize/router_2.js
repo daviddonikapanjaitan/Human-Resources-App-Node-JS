a5122ddfb4459d6e1ead2ae163e866ae2988caaa
User <daviddonikapanjaitan@gmail.com>
2020-11-23 23:22:39 +0700

    Add Employee Module

--- router_sequelize/router_2.js
+++ router_sequelize/router_2.js
@@ -77,7 +77,7 @@ router.post('/', async function(req, res, next){
             zip_code,
             picture,
         } = req.body;
-        var pictureBlob = Buffer.from(picture, "base64");
+        let pictureBlob = Buffer.from(picture, "base64");
         const add_employee = await model.employee.create({
             first_name,
             last_name,
@@ -116,6 +116,112 @@ router.post('/', async function(req, res, next){
                 'data':{}
             });
         }
+    }
+});
+
+// Delete by ID
+router.delete('/:id', async function (req, res, next) {
+    try {
+      const employee_id = req.params.id;
+      const employee = await model.employee.destroy({ where: {
+        id: employee_id
+      }})
+      if (employee) {
+        res.json({
+          'status': 'OK',
+          'messages': 'Employee has beend deleted',
+          'data': employee,
+        })
+      }
+    } catch (err) {
+      res.status(400).json({
+        'status': 'ERROR',
+        'messages': err.message,
+        'data': {},
+      })
+    }
+});
+
+// GET employee by ID
+router.get('/:id', async function(req, res, next){
+    try{
+        const employee = await model.employee.findAll({
+            where: {
+                id: parseInt(req.params.id)
+            }
+        });
+        if(employee){
+            res.json({
+                'status': 'OK',
+                'messages': '',
+                'count': employee
+            });
+        }
+    }catch(err){
+        if(err){
+            console.log(err);
+            res.json({
+                'status':'ERROR',
+                'messages':err.messages,
+                'data':{}
+            });
+        }
+    }
+});
+
+router.patch('/:id', async function (req, res, next) {
+    try {
+      const employee_id = req.params.id;
+      const {
+        first_name,
+        last_name,
+        designation,
+        phone,
+        rate_type,
+        hour_rate_salary,
+        email,
+        blood_group,
+        address_line_1,
+        address_line_2,
+        country,
+        city,
+        zip_code,
+        picture,
+      } = req.body;
+      let pictureBlob = Buffer.from(picture, "base64");
+      const update_employee = await model.employee.update({
+        first_name,
+        last_name,
+        designation,
+        phone: parseInt(phone),
+        rate_type,
+        hour_rate_salary,
+        email,
+        blood_group,
+        address_line_1,
+        address_line_2,
+        country,
+        city,
+        zip_code,
+        picture:pictureBlob
+      }, {
+        where: {
+          id: employee_id
+        }
+      });
+      if (update_employee) {
+        res.json({
+          'status': 'OK',
+          'messages': 'Employee has been updated',
+          'data': update_employee,
+        })
+      }
+    } catch (err) {
+      res.status(400).json({
+        'status': 'ERROR',
+        'messages': err.message,
+        'data': {},
+      })
     }
 });
 
